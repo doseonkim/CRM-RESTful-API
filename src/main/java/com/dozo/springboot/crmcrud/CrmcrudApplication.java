0ed37904350af2670b0ed4ad6d96fa4c558ad899
doseon <doseon@live.co.kr>
2019-02-04 20:09:26 -0800

    Initialize Spring project using Spring boot.

---
+++ src/main/java/com/dozo/springboot/crmcrud/CrmcrudApplication.java
@@ -0,0 +1,14 @@
+package com.dozo.springboot.crmcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmcrudApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/dozo/springboot/crmcrud/CrmcrudApplicationTests.java
@@ -0,0 +1,17 @@
+package com.dozo.springboot.crmcrud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CrmcrudApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
