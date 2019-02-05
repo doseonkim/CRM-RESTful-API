23571a6e066e82385be7c4609e2a24ee1a3f2919
doseon <doseon@live.co.kr>
2019-02-05 00:17:24 -0800

    Use spring data rest to handle controller. Remove controller and services. Switch to JPA Repository + spring boot data rest.

--- src/main/java/com/dozo/springboot/crmcrud/dao/EmployeeRepo.java
+++ src/main/java/com/dozo/springboot/crmcrud/dao/EmployeeRepo.java
@@ -1,9 +1,11 @@
 package com.dozo.springboot.crmcrud.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.dozo.springboot.crmcrud.entity.Employee;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepo extends JpaRepository<Employee, Integer> {
-
+	
 }
